84b9a735330eceab081a5349775883e06dfe25b1
Nathan Lucas <nathan@bnlucas.com>
2013-05-17 15:57:33 -0600

    initial push.

---
+++ README.md
@@ -0,0 +1,19 @@
+delimited_groups
+================
+
+This was made in answer to an API service only allowing a maximum of 10 items to
+be passed in a comma-delimited string.
+
+```python
+class caller(_service):
+
+	def add_tags(self, tags):
+		parameters = []
+		tag_groups = delimited_groups(tags, 10)
+		for i in tag_groups:
+			parameters.append({'tags': tag_group[i]})
+		return self.multi_request('post', 'image.add_tags', parameters)
+```python
+
+This will generate groups of 10 comma-delimited tags to be sent to the service.
+the .multi_request() creates requests for all parameters[n]
---
+++ delimited_groups.py
@@ -0,0 +1,24 @@
+# While working with an API service, they allow a comma-delimited list of values
+# with a maximum length of 10. While deciding whether or not to raise an error
+# if the length > 10 and block the call, I decided to make multiple calls to the
+# service.
+#
+# If n is omitted, this will create one group of all values.
+
+def delimited_groups(data, n=10, copy=False):
+    groups = []
+
+    if copy:
+        list_ = list(data)
+    else:
+        list_ = data
+    n = len(list_) if n == 0 else n
+
+    if len(list_) < n:
+        groups.append(','.join(list_))
+    else:
+        while len(list_) > 0:
+            groups.append(','.join(list_[:n]))
+            del list_[:n]
+
+    return groups
